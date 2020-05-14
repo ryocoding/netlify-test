ca679480b425eac02ecb627de5443245a93ac2e6
Ryo <thunderbolt.code@gmail.com>
2020-05-15 01:14:02 +0900

    added work page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+function Top() {
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        <Link to="/works">works</Link>
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  );
+}
+
+function Work() {
+  return (
+    <div>this is work page
+      <Link to="/">top</Link>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div>
+          <Route exact path='/' component={Top}/>
+          <Route path='/works' component={Work}/>
+        </div>
+      </Router>
     </div>
   );
 }
